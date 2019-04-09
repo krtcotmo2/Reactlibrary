248d0dcfd1490d3d93185aeb3a5f775442cd3780
krtcotmo2 <krtcotmo2@gmail.com>
2019-04-08 23:11:40 -0400

    build folder

---
+++ client/build/precache-manifest.b6cff6ec6fab5c1845e043f1f321b476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a3d18d9a780067a317c1",
+    "url": "/static/js/main.05c55174.chunk.js"
+  },
+  {
+    "revision": "d05161d5159a0e40a309",
+    "url": "/static/js/2.38a7e0c6.chunk.js"
+  },
+  {
+    "revision": "a3d18d9a780067a317c1",
+    "url": "/static/css/main.2bc1b643.chunk.css"
+  },
+  {
+    "revision": "d2d427fdbaf6ecd3e7bc09c2178236e7",
+    "url": "/index.html"
+  }
+];
